578023141d5bb07f6c1a84e2d65cd46117a1d1f8
Stefan Zerkalica <zerkalica@gmail.com>
2021-05-13 23:57:25 +0300

    fix: update ts 4.2.4

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import path from 'path'
 import * as ts_module from 'typescript/lib/tsserverlibrary'
-
 import { ConfigReader } from './ConfigReader'
 
 type TAutoImportProviderProject = ts_module.server.AutoImportProviderProject & {
@@ -12,12 +11,14 @@ type TAutoImportProviderProject = ts_module.server.AutoImportProviderProject & {
   ): string[]
 }
 
+class IsValidAcc {
+  isValid = false
+  constructor(readonly source: string) {}
+}
+
 function init({ typescript: ts }: { typescript: typeof ts_module }) {
   const configReader = new ConfigReader(ts)
-
   const monorepoRoots = new Set<string>()
-
-  let dist = 'dist'
 
   const AutoImportProviderProject = (ts.server
     .AutoImportProviderProject as unknown) as TAutoImportProviderProject
@@ -45,22 +46,17 @@ function init({ typescript: ts }: { typescript: typeof ts_module }) {
     const result: string[] = []
 
     for (const item of imports) {
-      result.push(item)
+      // result.push(item)
 
+      if (!item.endsWith('d.ts')) continue
       // Fix paths only from monorepo
       if (item.includes('node_modules')) continue
-      if (!item.endsWith('d.ts')) continue
 
       const itemPath = path.dirname(item)
 
       const config = configReader.get(itemPath)
       const outDir = config?.options.outDir ?? itemPath
       const rootDir = config?.options.rootDir ?? path.dirname(itemPath)
-
-      /**
-       * Autodetect dist from tsconfig
-       */
-      if (config?.options.outDir) dist = path.basename(outDir)
 
       /**
        * Save all monorepo project dependencies paths for filtering imports in getCompletionsAtPosition
@@ -95,18 +91,8 @@ function init({ typescript: ts }: { typescript: typeof ts_module }) {
 
     const currenDirectory = info.project.getCurrentDirectory()
 
-    function isValidImport(importPath: string) {
-      // Check paths only from monorepo
-      if (importPath.includes('node_modules')) return true
-      if (importPath.startsWith(currenDirectory)) return true
-
-      let isValid = false
-
-      monorepoRoots.forEach(root => {
-        if (importPath.startsWith(root)) isValid = true
-      })
-
-      return isValid
+    function isValidEntry(this: IsValidAcc, root: string) {
+      if (this.source.startsWith(root)) this.isValid = true
     }
 
     /**
@@ -115,49 +101,43 @@ function init({ typescript: ts }: { typescript: typeof ts_module }) {
      */
     proxy.getCompletionsAtPosition = (fileName, position, options) => {
       const prev = ls.getCompletionsAtPosition(fileName, position, options)
+
       if (!prev) return prev
+      const entries = prev.entries
+      const nextEntries = [] as typeof prev.entries
+      const added = new Set<string>()
+      for (let i = 0, l = entries.length; i < l; i++) {
+        const e = entries[i]
+        const source = e.source
+        if (source === undefined) {
+          nextEntries.push(e)
+          continue
+        }
+
+        if (added.has(source)) continue
+        added.add(source)
+
+        // Check paths only from monorepo
+        if (
+          e.isPackageJsonImport === undefined
+          || source.startsWith(currenDirectory)
+          || source.includes('node_modules')
+        ) {
+          nextEntries.push(e)
+          continue
+        }
+
+        const acc = new IsValidAcc(source)
+  
+        monorepoRoots.forEach(isValidEntry, acc)
+  
+        if (acc.isValid) nextEntries.push(e)
+      }
 
       return {
         ...prev,
-        entries: prev.entries.filter(
-          entry => !entry.source || !entry.isPackageJsonImport || isValidImport(entry.source)
-        ),
+        entries: nextEntries,
       }
-    }
-
-    const importRegEx = new RegExp('^(import .*[\"\'])(.*)([\"\'].*[\\n\\r]*)', 'm')
-
-    function removeImportDistPart(importStr: string) {
-      const parts = importStr.match(importRegEx)
-      if (! parts) return importStr
-
-      const newImport = parts[2].replace(`/${dist}/`, '/')
-
-      return `${parts[1]}${newImport}${parts[3]}`
-    }
-
-    /**
-     * Remove `/dist/` part from import (tsserver insert it, when no index.js reexport)
-     * https://github.com/microsoft/TypeScript/issues/40913#example-2
-     */
-    proxy.getCompletionEntryDetails = (fileName, position, entryName, formatOptions, src, preferences) => {
-      const prev = ls.getCompletionEntryDetails(fileName, position, entryName, formatOptions, src, preferences)
-      if (!prev || ! prev.codeActions || ! prev.source) return prev
-
-      const codeActions = prev.codeActions.map(action => ({
-        commands: action.commands,
-        description: action.description,
-        changes: action.changes.map(change => ({
-          fileName: change.fileName,
-          isNewFile: change.isNewFile,
-          textChanges: change.textChanges.map(textChange => ({
-            span: textChange.span,
-            newText: removeImportDistPart(textChange.newText)
-          }))
-        }))
-      }))
-      
-      return { ...prev, codeActions }
     }
 
     return proxy
